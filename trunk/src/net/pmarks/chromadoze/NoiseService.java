8c2f753647e75fbe6e55492fadae9a532e74e7cb
sparkmaul <sparkmaul@8aab93e6-1153-11df-a992-f145ad325602>
2011-01-10 02:43:42 +0000

    Run as a foreground service.  This makes the OS less likely to kill the process while it's busy making noise.

--- trunk/src/net/pmarks/chromadoze/NoiseService.java
+++ trunk/src/net/pmarks/chromadoze/NoiseService.java
@@ -17,8 +17,10 @@
 
 package net.pmarks.chromadoze;
 
-import junit.framework.Assert;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
+import junit.framework.Assert;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
@@ -41,10 +43,23 @@ public class NoiseService extends Service {
     private SampleGenerator mSampleGenerator;
     
     private static final int NOTIFY_ID = 1;
-    private NotificationManager mNotificationManager;
     private PowerManager.WakeLock mWakeLock;
     
     private Handler mPercentHandler;
+    
+    // Fields for {start,stop}ForegroundCompat().  See:
+    // http://developer.android.com/resources/samples/ApiDemos/src/com/example/android/apis/app/ForegroundService.html
+    @SuppressWarnings("unchecked")
+    private static final Class[] mStartForegroundSignature = new Class[] {
+        int.class, Notification.class};
+    @SuppressWarnings("unchecked")
+    private static final Class[] mStopForegroundSignature = new Class[] {
+        boolean.class};
+    private NotificationManager mNM;
+    private Method mStartForeground;
+    private Method mStopForeground;
+    private Object[] mStartForegroundArgs = new Object[2];
+    private Object[] mStopForegroundArgs = new Object[1];
     
     @Override
     public void onCreate() {
@@ -59,13 +74,23 @@ public class NoiseService extends Service {
         
         mSampleShuffler = new SampleShuffler();
         mSampleGenerator = new SampleGenerator(this, mSampleShuffler);
-        mNotificationManager =
-            (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
         PowerManager pm = (PowerManager)getSystemService(Context.POWER_SERVICE);
         mWakeLock = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "ChromaDoze Wake Lock");
         mWakeLock.acquire();
         
-        addNotify();
+        // Initialization junk for {start,stop}ForegroundCompat().
+        mNM = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+        try {
+            mStartForeground = getClass().getMethod("startForeground",
+                    mStartForegroundSignature);
+            mStopForeground = getClass().getMethod("stopForeground",
+                    mStopForegroundSignature);
+        } catch (NoSuchMethodException e) {
+            // Running on an older platform.
+            mStartForeground = mStopForeground = null;
+        }
+        
+        startForegroundCompat(NOTIFY_ID, makeNotify());
     }
 
     @Override
@@ -82,7 +107,7 @@ public class NoiseService extends Service {
         mPercentHandler.removeMessages(PERCENT_MSG);
         updatePercent(-1);
         
-        removeNotify();
+        stopForegroundCompat(NOTIFY_ID);
         mWakeLock.release();
     }
 
@@ -92,8 +117,8 @@ public class NoiseService extends Service {
         return null;
     }
     
-    // Display an icon in the notification bar.
-    private void addNotify() {
+    // Create an icon for the notification bar.
+    private Notification makeNotify() {
         int icon = R.drawable.stat_noise;
         long when = System.currentTimeMillis();
         Notification n = new Notification(icon, null, when);
@@ -106,11 +131,8 @@ public class NoiseService extends Service {
                 getString(R.string.app_name),
                 getString(R.string.select_to_configure),
                 contentIntent);
-        mNotificationManager.notify(NOTIFY_ID, n);
-    }
-    
-    private void removeNotify() {
-        mNotificationManager.cancel(NOTIFY_ID);
+        
+        return n;
     }
     
     // Call updatePercent() from any thread.
@@ -135,5 +157,53 @@ public class NoiseService extends Service {
     public static void setPercentListener(NoiseServicePercentListener listener) {
         sPercentListener = listener;
         updatePercent(sLastPercent);
+    }
+    
+    /**
+     * This is a wrapper around the new startForeground method, using the older
+     * APIs if it is not available.
+     */
+    void startForegroundCompat(int id, Notification notification) {
+        // If we have the new startForeground API, then use it.
+        if (mStartForeground != null) {
+            mStartForegroundArgs[0] = Integer.valueOf(id);
+            mStartForegroundArgs[1] = notification;
+            try {
+                mStartForeground.invoke(this, mStartForegroundArgs);
+            } catch (InvocationTargetException e) {
+                // Should not happen.
+            } catch (IllegalAccessException e) {
+                // Should not happen.
+            }
+            return;
+        }
+
+        // Fall back on the old API.
+        setForeground(true);
+        mNM.notify(id, notification);
+    }
+
+    /**
+     * This is a wrapper around the new stopForeground method, using the older
+     * APIs if it is not available.
+     */
+    void stopForegroundCompat(int id) {
+        // If we have the new stopForeground API, then use it.
+        if (mStopForeground != null) {
+            mStopForegroundArgs[0] = Boolean.TRUE;
+            try {
+                mStopForeground.invoke(this, mStopForegroundArgs);
+            } catch (InvocationTargetException e) {
+                // Should not happen.
+            } catch (IllegalAccessException e) {
+                // Should not happen.
+            }
+            return;
+        }
+
+        // Fall back on the old API.  Note to cancel BEFORE changing the
+        // foreground state, since we could be killed at that point.
+        mNM.cancel(id);
+        setForeground(false);
     }
 }
