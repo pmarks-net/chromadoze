b76d9b0589b5313e164132c2df22e7648d320bd7
sparkmaul <sparkmaul@8aab93e6-1153-11df-a992-f145ad325602>
2013-08-04 10:38:43 +0000

    - Merge in DragSortListView - Partially implement a UI for the "Memory" fragment.

---
+++ trunk/src/net/pmarks/chromadoze/MemoryArrayAdapter.java
@@ -0,0 +1,57 @@
+// Copyright (C) 2013  Paul Marks  http://www.pmarks.net/
+//
+// This file is part of Chroma Doze.
+//
+// Chroma Doze is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Chroma Doze is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Chroma Doze.  If not, see <http://www.gnu.org/licenses/>.
+
+package net.pmarks.chromadoze;
+
+import java.util.List;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+public class MemoryArrayAdapter extends ArrayAdapter<String> {
+
+    private final Context mContext;
+
+    public MemoryArrayAdapter(Context context, int resource,
+            int textViewResourceId, List<String> objects) {
+        super(context, resource, textViewResourceId, objects);
+        mContext = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) mContext
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View view;
+        if (convertView == null) {
+            view = inflater.inflate(R.layout.memory_list_item, parent, false);
+        } else {
+            view = convertView;
+        }
+
+        TextView text = (TextView) view.findViewById(R.id.text);
+        text.setText(getItem(position));
+
+        return view;
+
+    }
+}
