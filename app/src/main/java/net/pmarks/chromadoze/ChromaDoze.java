0090caa083dfd50f5ea12637cc75b4435925c2fa
Paul Marks <paul@pmarks.net>
2016-01-20 22:17:05 -0800

    Reenable the backupAgent, for testing.

--- app/src/main/java/net/pmarks/chromadoze/ChromaDoze.java
+++ app/src/main/java/net/pmarks/chromadoze/ChromaDoze.java
@@ -17,6 +17,7 @@
 
 package net.pmarks.chromadoze;
 
+import android.app.backup.BackupManager;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.PorterDuff.Mode;
@@ -128,6 +129,7 @@ public class ChromaDoze extends AppCompatActivity implements
         pref.clear();
         mUiState.saveState(pref);
         pref.commit();
+        new BackupManager(this).dataChanged();
 
         // Stop receiving progress events.
         NoiseService.removePercentListener(this);
